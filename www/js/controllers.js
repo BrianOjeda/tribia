8062ff1f64248a0420bb5bd09a26112b3660a330
BrianOjeda <brian_ezequiel_ojeda@hotmail.com>
2016-09-23 11:10:52 -0300

    Terminado, faltan detalles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,42 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+
+  function login()
+  {
+    setCookie('user',$("#userName").val(),1);
+    setCookie('pass',$("#userPassword").val(),1);
+
+   location.href="#/tab/chats";
+  }
+
+  $scope.login=login;
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -15,6 +49,7 @@ angular.module('starter.controllers', [])
   //$scope.remove = function(chat) {
    // Chats.remove(chat);
   //};
+  $scope.user=getCookie('user');
   $scope.tribia=[];
   //$scope.respuestas=[];
   var contador=0;
